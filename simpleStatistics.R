0f06521082845bc35ea3daa6afa2348344eb78be
Carlos Mauricio <c.mauricio1985@gmail.com>
2015-11-27 15:19:26 +0100

    commit151127

--- simpleStatistics.R
+++ simpleStatistics.R
@@ -12,12 +12,12 @@
 
 #####Getting the data#####
 
-wDirectory<-"C:/Users/Mauro/Dropbox/ETDataAnalysis"
-csvFile<-"151105-FullDB.csv"
+wDirectory<-"C:/Users/ru25tas/Desktop/Analysis"
+csvFile<-"151126-LoLExpertsETDatabase.csv"
 
 setwd(wDirectory)
 
-fullDatabase<-read.csv2(csvFile, header=TRUE, sep=";", dec=",")
+fullDatabase<-read.csv2(csvFile, header=TRUE, sep=";", dec=",",na.strings="NA")
 fullDatabase<-as.data.frame(fullDatabase) 
 
 fullDatabase[,"Session"]<-as.factor(fullDatabase[,"Session"])
@@ -25,14 +25,14 @@ fullDatabase[,"Age"]<-as.integer(fullDatabase[,"Age"])
 fullDatabase[,"Gender"]<-as.factor(fullDatabase[,"Gender"])
 fullDatabase[,"Group"]<-as.factor(fullDatabase[,"Group"])
 fullDatabase[,"Expertise"]<-as.factor(fullDatabase[,"Expertise"])
-fullDatabase[,"Condition"]<-as.integer(fullDatabase[,"Condition"])
+fullDatabase[,"Condition"]<-as.factor(fullDatabase[,"Condition"])
 fullDatabase[,"Game"]<-as.factor(fullDatabase[,"Game"])
 fullDatabase[,"Tier"]<-factor(fullDatabase[,"Tier"], order=T, levels=c("bronce", "silver", "gold", "platinum", "diamond"))
 fullDatabase[,"Division"]<-factor(fullDatabase[,"Division"], order=T, levels=c("1","2","3","4","5"))
 fullDatabase[,"GroupByTier"]<-factor(fullDatabase[,"GroupByTier"], order=T, levels=c("A","B","C","D","E","F","G"))
 fullDatabase[,"Elo"]<-as.integer(fullDatabase[,"Elo"])
 fullDatabase[,"GroupByElo"]<-factor(fullDatabase[,"GroupByElo"], order=T, levels=c("A","B","C","D","E"))
-fullDatabase[,"Outcome"]<-factor(fullDatabase[,"Outcome"], order=T, levels=c("lose","win"))
+fullDatabase[,"Outcome"]<-factor(fullDatabase[,"Outcome"], order=T, levels=c("lost","win"))
 
 n<-which(colnames(fullDatabase)=="GazeGame.MM")
 
@@ -41,48 +41,48 @@ n<-which(colnames(fullDatabase)=="GazeGame.MM")
 
 #####Gatting the game Time#####
 
-gameTimeTierDB<-cbind(fullDatabase$GroupByTier,fullDatabase[,40],fullDatabase[,41],fullDatabase[,42],fullDatabase[43])
+gameTimeTierDB<-cbind(fullDatabase$GroupByTier, fullDatabase$Condition, fullDatabase$Outcome, fullDatabase$SurfaceVisGame.secs.,fullDatabase$SurfaceVisMM.secs.,fullDatabase$TotalSurfaceVisTotal.secs.,fullDatabase$VisibilityLost..secs.)
 #gameTimeTierDB<-na.omit(gameTimeTierDB)
 gameTimeTierDB<-as.data.frame(gameTimeTierDB)
-colnames(gameTimeTierDB)<-c("GroupByTier","SurfaceVisGameSecs","SurfaceVisMMSecs","SurfaceVisTotalSecs","SurfaceVislostSecs")
+colnames(gameTimeTierDB)<-c("GroupByTier", "Condition", "Outcome", "SurfaceVisGameSecs","SurfaceVisMMSecs","SurfaceVisTotalSecs","SurfaceVislostSecs")
 
-plot(gameTimeTierDB)
+plot(gameTimeTierDB, col=fullDatabase$GroupByTier)
 
 #####Percentage of Gaze by Tier#####
 
-percGazeDB<-cbind(fullDatabase$GroupByTier,fullDatabase[,37],fullDatabase[,38],fullDatabase[,39])
+percGazeDB<-cbind(fullDatabase$GroupByTier,fullDatabase$PercGazeGame,fullDatabase$Condition, fullDatabase$Outcome, fullDatabase$PercGazeMM,fullDatabase$PercGazeOut)
 #percGazeDB<-na.omit(percGazeDB)
 percGazeDB<-as.data.frame(percGazeDB)
-colnames(percGazeDB)<-c("GroupByTier","PercGazeGame","PercGazeMM","PercGazeout")
+colnames(percGazeDB)<-c("GroupByTier", "Condition", "Outcome", "PercGazeGame","PercGazeMM","PercGazeout")
 
-plot(percGazeDB)
+plot(percGazeDB, col=fullDatabase$GroupByTier)
 
 #####Number of fixations by Tier#####
 
-numberFixationsDB<-cbind(fullDatabase$GroupByTier,fullDatabase$NrFixationsGame, fullDatabase$NrFixNOMM,fullDatabase$NrFixationsMM)
+numberFixationsDB<-cbind(fullDatabase$GroupByTier, fullDatabase$Condition, fullDatabase$Outcome, fullDatabase$NrFixationsGame, fullDatabase$NrFixNOMM,fullDatabase$NrFixationsMM)
 #numberFixationsDB<-na.omit(numberFixationsDB)
 numberFixationsDB<-as.data.frame(numberFixationsDB)
-colnames(numberFixationsDB)<-c("GroupByTier", "TotalFixationsGame", "NrFixationsMap","NrFixationsMM")
+colnames(numberFixationsDB)<-c("GroupByTier", "Condition", "Outcome", "TotalFixationsGame", "NrFixationsMap","NrFixationsMM")
 
-plot(numberFixationsDB)
+plot(numberFixationsDB, col=fullDatabase$GroupByTier)
 
 #####Number of Saccades by Tier#####
 
-numberSaccadesDB<-cbind(fullDatabase$GroupByTier,fullDatabase$SaccadesMap,fullDatabase$SaccadesMM)
+numberSaccadesDB<-cbind(fullDatabase$GroupByTier, fullDatabase$Condition, fullDatabase$Outcome, fullDatabase$SaccadesMap,fullDatabase$SaccadesMM)
 #numberSaccadesDB<-na.omit(numberSaccadesDB)
 numberSaccadesDB<-as.data.frame(numberSaccadesDB)
-colnames(numberSaccadesDB)<-c("GroupByTier","SaccadesGame","SaccadesMM")
+colnames(numberSaccadesDB)<-c("GroupByTier", "Condition", "Outcome", "SaccadesGame","SaccadesMM")
 
-plot(numberSaccadesDB)
+plot(numberSaccadesDB, col=fullDatabase$GroupByTier)
 
 #####relation saccade-fixation by Tier#####
 
-SacFixRatioDB<-cbind(fullDatabase$GroupByTier,fullDatabase$SacFixRatioMap, fullDatabase$SacFixRatioMM)
+SacFixRatioDB<-cbind(fullDatabase$GroupByTier,fullDatabase$Condition, fullDatabase$Outcome, fullDatabase$SacFixRatioMap, fullDatabase$SacFixRatioMM)
 #SacFixRatioDB<-na.omit(SacFixRatioDB)
 SacFixRatioDB<-as.data.frame(SacFixRatioDB)
-colnames(SacFixRatioDB)<-c("GroupByTier","SacFixRatioGame","SacFixRatioMM")
+colnames(SacFixRatioDB)<-c("GroupByTier", "Condition", "Outcome", "SacFixRatioGame","SacFixRatioMM")
 
-plot(SacFixRatioDB)
+plot(SacFixRatioDB, col=fullDatabase$GroupByTier)
 
 
 #####Average (statistical mean) of fixations and saccades per game#####
@@ -90,31 +90,31 @@ plot(SacFixRatioDB)
 meanSaccadesMap<-numberSaccadesDB$SaccadesGame/gameTimeTierDB$SurfaceVisGameSecs
 meanSaccadesMM<-numberSaccadesDB$SaccadesMM/gameTimeTierDB$SurfaceVisMMSecs
 
-meanSacsFixTier<-cbind(fullDatabase$GroupByTier,fullDatabase$MeanFixationsGame, meanSaccadesMap, fullDatabase$MeanFixationsMM, meanSaccadesMM)
+meanSacsFixTier<-cbind(fullDatabase$GroupByTier, fullDatabase$Condition, fullDatabase$Outcome, fullDatabase$MeanFixationsGame, meanSaccadesMap, fullDatabase$MeanFixationsMM, meanSaccadesMM)
 meanSacsFixTier<-as.data.frame(meanSacsFixTier)
 
-colnames(meanSacsFixTier)<-c("GroupByTier","MeanFixMap","MeanSacMap", "MeanFixMM", "MeanSacMM")
+colnames(meanSacsFixTier)<-c("GroupByTier","Condition", "Outcome", "MeanFixMap","MeanSacMap", "MeanFixMM", "MeanSacMM")
 
-plot(meanSacsFixTier)
+plot(meanSacsFixTier, col=fullDatabase$GroupByTier)
 
 with(meanSacsFixTier, plot(MeanFixMM,MeanSacMM, col = GroupByTier)) #Makes a plot with colour based on the Tier
 abline(h = 12, lwd = 2, lty = 2)
 
 ####Mean of fixation duration#####
 
-meanFixTime<-cbind(fullDatabase$GroupByTier,fullDatabase$MeanDurationGame, fullDatabase$MeanDurationMM)
+meanFixTime<-cbind(fullDatabase$GroupByTier, fullDatabase$Condition, fullDatabase$Outcome, fullDatabase$MeanDurationGame, fullDatabase$MeanDurationMM)
 meanFixTime<-as.data.frame(meanFixTime)
 
-colnames(meanFixTime)<-c("GroupByTier","MeanFixDurationMap","MeanFixDurationMM")
+colnames(meanFixTime)<-c("GroupByTier","Condition", "Outcome", "MeanFixDurationMap","MeanFixDurationMM")
 
-plot(meanFixTime)
+plot(meanFixTime, col=fullDatabase$GroupByTier)
 
 #####Percentage of fixations according to time#####
 
-perc1<-avgSacsFixTierTime$AvgFixMap*numberFixationsDB$NrFixationsMap
-perc<-(sqrt(perc1)/sqrt(gameTimeTierDB$SurfaceVisGameSecs)) #bad
+#perc1<-avgSacsFixTierTime$AvgFixMap*numberFixationsDB$NrFixationsMap
+#perc<-(sqrt(perc1)/sqrt(gameTimeTierDB$SurfaceVisGameSecs)) #bad
 
-View(perc1)
+#View(perc1)
 
 
 #####Percentage of gaze according to time#####
@@ -127,25 +127,38 @@ View(perc1)
 #####Print exploratory plots##### 
 
 png('GameTierExpl.png', width = 1024, height = 768, units = "px", pointsize = 13)
-plot(gameTimeTierDB)
+plot(gameTimeTierDB, col=fullDatabase$GroupByTier)
 dev.off()
 png('PercGazeExpl.png', width = 1024, height = 768, units = "px", pointsize = 13)
-plot(percGazeDB)
+plot(percGazeDB, col=fullDatabase$GroupByTier)
 dev.off()
 png('NumberFixationsExpl.png', width = 1024, height = 768, units = "px", pointsize = 13)
-plot(numberFixationsDB)
+plot(numberFixationsDB, col=fullDatabase$GroupByTier)
 dev.off()
 png('NumberSaccadesExpl.png', width = 1024, height = 768, units = "px", pointsize = 13)
-plot(numberSaccadesDB)
+plot(numberSaccadesDB, col=fullDatabase$GroupByTier)
 dev.off()
 png('MeanSaccades.png', width = 1024, height = 768, units = "px", pointsize = 13)
-plot(meanSacsFixTier)
+plot(meanSacsFixTier, col=fullDatabase$GroupByTier)
 dev.off()
 png('MeanFixations.png', width = 1024, height = 768, units = "px", pointsize = 13)
-plot(meanFixTime)
+plot(meanFixTime, col=fullDatabase$GroupByTier)
+dev.off()
+png('SacFixRatio.png', width = 1024, height = 768, units = "px", pointsize = 13)
+plot(SacFixRatioDB, col=fullDatabase$GroupByTier)
 dev.off()
 
+#####Next analyses by tier and game#####
+
+
+
 ########################
+########################
+########################
+########################
+########################
+########################
+
 png('saccadesmap.png', width = 1024, height = 768, units = "px", pointsize = 16) #Opens the writing machine
 
 explore<-par(mfcol=c(2,3)) #plot various plots in one sheet
