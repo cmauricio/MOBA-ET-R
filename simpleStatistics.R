3557ceb8d2db88817abc61dde4f2c7a36a4c4cd0
Carlos Mauricio <c.mauricio1985@gmail.com>
2015-12-01 16:00:22 +0100

    Commit151201

--- simpleStatistics.R
+++ simpleStatistics.R
@@ -1,25 +1,14 @@
-#b<-tapply(fullDatabase$NrFixationsGameT1, fullDatabase$Tier, mean)
-#View(c)
-#aggregate(verbal, by=list(schule,sex), FUN=mean)#it tells to separate the data in as many variables as we want
-#aov(verbal~sex*schule)#perform an analysis of variables (the first factor is the dependant)
-#barplot(table(schule)/length(schule))
-#hist(iq) # Makes a histogram of the variable sampling the variable automatically
-#a<-subset(fullDatabase, subset=Tier=="silver")
-#Excluding missing values from analyses
-#na.rm=T
-# create new dataset without missing data 
-#newdata <- na.omit(mydata)
-
 #####Getting the data#####
 
 wDirectory<-"C:/Users/ru25tas/Desktop/Analysis"
-csvFile<-"151126-LoLExpertsETDatabase.csv"
+csvFile<-"151201-lolExpertsDB.csv"
 
 setwd(wDirectory)
 
 fullDatabase<-read.csv2(csvFile, header=TRUE, sep=";", dec=",",na.strings="NA")
 fullDatabase<-as.data.frame(fullDatabase) 
 
+# General fields
 fullDatabase[,"Session"]<-as.factor(fullDatabase[,"Session"])
 fullDatabase[,"Age"]<-as.integer(fullDatabase[,"Age"])
 fullDatabase[,"Gender"]<-as.factor(fullDatabase[,"Gender"])
@@ -27,17 +16,54 @@ fullDatabase[,"Group"]<-as.factor(fullDatabase[,"Group"])
 fullDatabase[,"Expertise"]<-as.factor(fullDatabase[,"Expertise"])
 fullDatabase[,"Condition"]<-as.factor(fullDatabase[,"Condition"])
 fullDatabase[,"Game"]<-as.factor(fullDatabase[,"Game"])
+fullDatabase[,"Outcome"]<-factor(fullDatabase[,"Outcome"], order=T, levels=c("lost","win"))
+
+# Only LoL Experts fields
 fullDatabase[,"Tier"]<-factor(fullDatabase[,"Tier"], order=T, levels=c("bronce", "silver", "gold", "platinum", "diamond"))
 fullDatabase[,"Division"]<-factor(fullDatabase[,"Division"], order=T, levels=c("1","2","3","4","5"))
 fullDatabase[,"GroupByTier"]<-factor(fullDatabase[,"GroupByTier"], order=T, levels=c("A","B","C","D","E","F","G"))
+
+# Only DOTA fields
 fullDatabase[,"Elo"]<-as.integer(fullDatabase[,"Elo"])
 fullDatabase[,"GroupByElo"]<-factor(fullDatabase[,"GroupByElo"], order=T, levels=c("A","B","C","D","E"))
-fullDatabase[,"Outcome"]<-factor(fullDatabase[,"Outcome"], order=T, levels=c("lost","win"))
-
-n<-which(colnames(fullDatabase)=="GazeGame.MM")
 
 
-#####Spliting databases#####
+fullDatabase<-na.omit(fullDatabase)  # listwise deletion of missing values
+
+
+#####Spliting databases for LoL Experts#####
+
+group_a<-subset(fullDatabase, GroupByTier=="A")
+group_b<-subset(fullDatabase, GroupByTier=="B")
+group_c<-subset(fullDatabase, GroupByTier=="C")
+group_d<-subset(fullDatabase, GroupByTier=="D")
+group_e<-subset(fullDatabase, GroupByTier=="E")
+group_f<-subset(fullDatabase, GroupByTier=="F")
+group_g<-subset(fullDatabase, GroupByTier=="G")
+
+group_silver<-subset(fullDatabase, Tier=="silver")
+group_gold<-subset(fullDatabase, Tier=="gold")
+group_diamond<-subset(fullDatabase, Tier=="diamond")
+
+expert_expertDB<-subset(fullDatabase, Condition=="expert")
+expert_noviceDB<-subset(fullDatabase, Condition=="novice")
+
+
+#####Spliting databases for DOTA Experts#####
+
+group_a<-subset(fullDatabase, GroupByTier=="A")
+group_b<-subset(fullDatabase, GroupByTier=="B")
+group_c<-subset(fullDatabase, GroupByTier=="C")
+group_d<-subset(fullDatabase, GroupByTier=="D")
+group_e<-subset(fullDatabase, GroupByTier=="E")
+
+#####Spliting databases for LoL Novices#####
+
+expert_tutorialDB<-subset(fullDatabase, Condition=="tutorial")
+expert_noviceDB<-subset(fullDatabase, Condition=="novice")
+expert_ev1DB<-subset(fullDatabase, Condition=="evaluation1")
+expert_ev2DB<-subset(fullDatabase, Condition=="evaluation2")
+
 
 #####Gatting the game Time#####
 
@@ -83,7 +109,6 @@ SacFixRatioDB<-as.data.frame(SacFixRatioDB)
 colnames(SacFixRatioDB)<-c("GroupByTier", "Condition", "Outcome", "SacFixRatioGame","SacFixRatioMM")
 
 plot(SacFixRatioDB, col=fullDatabase$GroupByTier)
-
 
 #####Average (statistical mean) of fixations and saccades per game#####
 
@@ -148,7 +173,23 @@ png('SacFixRatio.png', width = 1024, height = 768, units = "px", pointsize = 13)
 plot(SacFixRatioDB, col=fullDatabase$GroupByTier)
 dev.off()
 
-#####Next analyses by tier and game#####
+##### Next analyses by tier and game #####
+
+
+
+##### Descriptives and plots #####
+
+plot(fullDatabase$Gender)
+plot(fullDatabase$Age)
+plot(fullDatabase$Group)
+plot(fullDatabase$GroupByTier)
+plot(fullDatabase$Condition)
+
+
+#hist(iq) # Makes a histogram of the variable sampling the variable automatically
+
+
+
 
 
 
@@ -159,102 +200,95 @@ dev.off()
 ########################
 ########################
 
-png('saccadesmap.png', width = 1024, height = 768, units = "px", pointsize = 16) #Opens the writing machine
+##### Other analyses #####
 
-explore<-par(mfcol=c(2,3)) #plot various plots in one sheet
+#means and standard deviations, including the range for valid data (+/- 3 sd)
 
-plot(fullDatabase$SaccadesMapT1~fullDatabase$Tier, main="saccades1")#The tilde is made with alt+126. At the left of the Tilde there is the dependant variable. An * between factors (in the right) is to make analysis of diferent factor interactions
-abline(lm(fullDatabase$SaccadesMapT1~fullDatabase$Tier), col="red")
-lines(lowess(fullDatabase$SaccadesMapT1~fullDatabase$Tier, f=2/3, iter=10), col="blue")
+mn_game<-mean(fullDatabase$NrFixNOMM,na.rm=T)
+sd_game<-sd(fullDatabase$NrFixNOMM, na.rm=T)
+range_sd_game<-c(mn_game-(3*sd_game),mn_game+(3*sd_game))
 
-plot(fullDatabase$SaccadesMapT2~fullDatabase$Tier, main="saccades2")
-abline(lm(fullDatabase$SaccadesMapT2~fullDatabase$Tier), col="red")
-lines(lowess(fullDatabase$SaccadesMapT2~fullDatabase$Tier, f=2/3, iter=10), col="blue")
+mn_mm<-mean(fullDatabase$NrFixationsMM,na.rm=T)
+sd_mm<-sd(fullDatabase$NrFixationsMM, na.rm=T)
+range_sd_mm<-c(mn_mm-(3*sd_mm),mn_mm+(3*sd_mm))
 
-plot(fullDatabase$SaccadesMapT3~fullDatabase$Tier, main="saccades3")
-abline(lm(fullDatabase$SaccadesMapT3~fullDatabase$Tier), col="red")
-lines(lowess(fullDatabase$SaccadesMapT3~fullDatabase$Tier, f=2/3, iter=10), col="blue")
+mn_game_sac<-mean(fullDatabase$SaccadesMap,na.rm=T)
+sd_game_sac<-sd(fullDatabase$SaccadesMap, na.rm=T)
+range_sd_sac_game<-c(mn_game_sac-(3*sd_game_sac),mn_game_sac+(3*sd_game_sac))
 
-plot(fullDatabase$SaccadesMapT4~fullDatabase$Tier, main="saccades4")
-abline(lm(fullDatabase$SaccadesMapT4~fullDatabase$Tier), col="red")
-lines(lowess(fullDatabase$SaccadesMapT4~fullDatabase$Tier, f=2/3, iter=10), col="blue")
+mn_mm_sac<-mean(fullDatabase$SaccadesMM,na.rm=T)
+sd_mm_sac<-sd(fullDatabase$SaccadesMM, na.rm=T)
+range_sd_sac_mm<-c(mn_mm_sac-(3*sd_mm_sac),mn_mm_sac+(3*sd_mm_sac))
 
-plot(fullDatabase$SaccadesMapT5~fullDatabase$Tier, main="saccades5")
-abline(lm(fullDatabase$SaccadesMapT5~fullDatabase$Tier), col="red")
-lines(lowess(fullDatabase$SaccadesMapT5~fullDatabase$Tier, f=2/3, iter=10), col="blue")
+mn_game_rat<-mean(fullDatabase$SacFixRatioMap,na.rm=T)
+sd_game_rat<-sd(fullDatabase$SacFixRatioMap, na.rm=T)
+range_sd_sac_rat<-c(mn_game_rat-(3*sd_game_rat),mn_game_rat+(3*sd_game_rat))
 
-plot(fullDatabase$SaccadesMapT6~fullDatabase$Tier, main="saccades6")
-abline(lm(fullDatabase$SaccadesMapT6~fullDatabase$Tier), col="red")
-lines(lowess(fullDatabase$SaccadesMapT6~fullDatabase$Tier, f=2/3, iter=10), col="blue")
+mn_mm_rat<-mean(fullDatabase$SacFixRatioMM,na.rm=T)
+sd_mm_rat<-sd(fullDatabase$SacFixRatioMM, na.rm=T)
+range_sd_rat_mm<-c(mn_mm_rat-(3*sd_mm_rat),mn_mm_rat+(3*sd_mm_rat))
 
-par(explore)
-dev.off() #closes the writing device
+mn_vision_lost<-mean(fullDatabase$VisionLostPercentage,na.rm=T)
+sd_vision_lost<-sd(fullDatabase$VisionLostPercentage,na.rm=T)
+range_vision_lost<-c(mn_vision_lost-(3*sd_vision_lost),(mn_vision_lost+(3*sd_vision_lost)))
 
-#######################################
 
-#to make a scatterplot  with a regression line and a smooth regression line
-plot(fullDatabase$SacFixRatioGameT1 ~fullDatabase$Tier)
-abline(lm(fullDatabase$SacFixRatioGameT1~fullDatabase$Tier), col="red")
-lines(lowess(fullDatabase$SacFixRatioGameT1~fullDatabase$Tier, f=2/3, iter=10), col="blue")
-
-plot(fullDatabase$SacFixRatioGameT1)
-
-b<-tapply(fullDatabase$NrFixationsGameT1, fullDatabase$Tier, mean)
-c<-tapply(fullDatabase$NrFixationsGameT2, fullDatabase$Tier, mean)
-
-plot(b,c)
-
-fit<-aov(fullDatabase$SacFixRatioGameT1~fullDatabase$Tier*fullDatabase$Gender)
-
-plot(fit)
-
-###############################
-
-png('saccadesmap2.png', width = 1024, height = 768, units = "px", pointsize = 13) #Opens the writing machine
-
-explore<-par(mfcol=c(4,6))
-
-plot(fullDatabase$SacFixRatioGameT1~fullDatabase$Tier*fullDatabase$Age*fullDatabase$Gender*fullDatabase$Group)
-plot(fullDatabase$SacFixRatioGameT2~fullDatabase$Tier*fullDatabase$Age*fullDatabase$Gender*fullDatabase$Group)
-plot(fullDatabase$SacFixRatioGameT3~fullDatabase$Tier*fullDatabase$Age*fullDatabase$Gender*fullDatabase$Group)
-plot(fullDatabase$SacFixRatioGameT4~fullDatabase$Tier*fullDatabase$Age*fullDatabase$Gender*fullDatabase$Group)
-plot(fullDatabase$SacFixRatioGameT5~fullDatabase$Tier*fullDatabase$Age*fullDatabase$Gender*fullDatabase$Group)
-plot(fullDatabase$SacFixRatioGameT6~fullDatabase$Tier*fullDatabase$Age*fullDatabase$Gender*fullDatabase$Group)
-
-par(explore)
-dev.off()
-
-png('saccadesmap3.png', width = 1024, height = 768, units = "px", pointsize = 13) #Opens the writing machine
-
-explore<-par(mfcol=c(4,6))
-
-plot(fullDatabase$SacFixRatioMMT1~fullDatabase$Tier*fullDatabase$Age*fullDatabase$Gender*fullDatabase$Group)
-plot(fullDatabase$SacFixRatioMMT2~fullDatabase$Tier*fullDatabase$Age*fullDatabase$Gender*fullDatabase$Group)
-plot(fullDatabase$SacFixRatioMMT3~fullDatabase$Tier*fullDatabase$Age*fullDatabase$Gender*fullDatabase$Group)
-plot(fullDatabase$SacFixRatioMMT4~fullDatabase$Tier*fullDatabase$Age*fullDatabase$Gender*fullDatabase$Group)
-plot(fullDatabase$SacFixRatioMMT5~fullDatabase$Tier*fullDatabase$Age*fullDatabase$Gender*fullDatabase$Group)
-plot(fullDatabase$SacFixRatioMMT6~fullDatabase$Tier*fullDatabase$Age*fullDatabase$Gender*fullDatabase$Group)
-
-par(explore)
-dev.off()
-
-###########################
-
-tbl<-table(fullDatabase$Tier,fullDatabase$SacFixRatioGameT1)
-tbl
-
-chisq.test()
-
-############################
-
-polyserial(x=fullDatabase$NrFixationsGameT1, y=fullDatabase$Tier, ML=T, std.err=T, maxcor=.9999, bins=4)
-
-#######################
+#b<-tapply(fullDatabase$NrFixationsGameT1, fullDatabase$Tier, mean)
+#aggregate(verbal, by=list(schule,sex), FUN=mean)#it tells to separate the data in as many variables as we want
+#aov(verbal~sex*schule)#perform an analysis of variables (the first factor is the dependant)
 
 
 
 
+##### Chi squared ######
 
+chisq.test(expert_expertDB$NrFixNOMM,expert_noviceDB$NrFixationsMM)
+
+##### Linear regressions #####
+
+Linear1<-lm(fullDatabase$NrFixNOMM ~ fullDatabase$NrFixationsMM)
+summary(Linear1)
+plot(Linear1)
+
+plot(fullDatabase$NrFixNOMM ~ fullDatabase$NrFixationsMM)
+abline(Linear1, col="red")
+
+
+##### Cluster analyses #####
+
+scaled_data<-scale(no_na_data$NrFixNOMM)# standardize variables
+
+library(mclust)
+fit <- Mclust(scaled_data)
+plot(fit) # plot results 
+summary(fit) # display the best model
+
+
+# K-Means Clustering with 5 clusters
+fit <- kmeans(scaled_data, 5)
+
+# Cluster Plot against 1st 2 principal components
+
+# vary parameters for most readable graph
+library(cluster) 
+clusplot(scaled_data, fit$cluster, color=TRUE, shade=TRUE, labels=2, lines=0)
+
+# Centroid Plot against 1st 2 discriminant functions
+library(fpc)
+plotcluster(scaled_data, fit$cluster)
+
+# comparing 2 cluster solutions
+library(fpc)
+cluster.stats(d, fit1$cluster, fit2$cluster)
+
+##### Anova ######
+
+anov_1<-aov(group_a$NrFixNOMM ~ group_a$NrFixationsMM)
+summary(anov_1)
+plot(anov_1)
+boxplot(group_a$NrFixNOMM ~ group_a$NrFixationsMM)
+
+########
 
 
 
